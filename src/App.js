cdef67c7b1467d02af6927c0d82cceb81b97c8bf
Amin <fakabbir.amin@siemens.com>
2021-04-03 21:41:25 +0530

    Adds tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,56 +1,83 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
-
+import React from "react";
+import sedansvg from "./icon-sedans.svg";
+import suvsvg from "./icon-suvs.svg";
+import luxurysvg from "./icon-luxury.svg";
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+    <React.Fragment>
+      <div className="mx-52 my-24">
+        <div className="grid grid-cols-3">
+          <div className="custom-col-1 rounded-tl-xl rounded-bl-xl pb-12">
+            <img src={sedansvg} className="w-16 object-contain my-12 mx-12" />
+            <h1
+              className="uppercase font-bold text-white text-3xl mx-12 font-sans"
+              // style={{ fontFamily: "Big Shoulders Display" }}
+            >
+              Sedans
+            </h1>
+            <p
+              className="my-12 mx-12 text-white font-bold font-sans"
+              style={{}}
+            >
+              Choose a sedan for its affordability and excellent fuel economy.
+              Ideal for cruising in the city or on your next road trip.
+            </p>
+
+            <div className="">
+              <button className="bg-white rounded-full mx-12 p-2 px-6 font-semibold">
+                Learn More
+              </button>
+            </div>
+          </div>
+
+          <div className="custom-col-2 ">
+            <img src={suvsvg} className="w-16 object-contain my-12 mx-12" />
+            <h1
+              className="uppercase font-bold text-white text-3xl mx-12"
+              style={{ fontFamily: "Big Shoulders Display" }}
+            >
+              SUVs
+            </h1>
+            <p className="my-12 mx-12 text-white">
+              Take an SUV for its spacious interior, power, and versatility.
+              Perfect for your next family vacation and off-road adventures.
+            </p>
+            <div className="">
+              <button className="bg-white rounded-full p-2 mx-12 px-6">
+                Learn More
+              </button>
+            </div>
+          </div>
+
+          <div className="custom-col-3 rounded-tr-xl rounded-br-xl">
+            <img src={luxurysvg} className="w-16 object-contain my-12 mx-12" />
+            <h1
+              className="uppercase font-bold text-white text-3xl mx-12"
+              style={{ fontFamily: "Big Shoulders Display" }}
+            >
+              Luxury
+            </h1>
+            <p className="my-12 mx-12 text-white">
+              Cruise in the best car brands without the bloated prices. Enjoy
+              the enhanced comfort of a luxury rental and arrive in style.
+            </p>
+            <div className="">
+              <button className="bg-white rounded-full p-2 px-6 mx-12">
+                Learn More
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Fakabbir Amin</a>.
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
